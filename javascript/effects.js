4a45fc16b3ec4bb56217d50380b01064e7102799
Jeison Guerrero <jei_son1516@hotmail.com>
2021-04-18 19:51:58 -0400

    final bookstore webpage

--- javascript/effects.js
+++ javascript/effects.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
 
+    $(".source li").draggable({
+        helper:"clone"
+    });
+    $("#cart").droppable({
+        accept: ".source > li",
+
+        drop:function(event,ui){
+            var droppedItem = $(ui.draggable).clone();
+            $(this).append(droppedItem)
+        }
+    });
+
+    $("#membershipSubmit").click(function(){
+        $(".joinMembership").addClass("important joinMembershipActive");;
+      });
     
 });  
